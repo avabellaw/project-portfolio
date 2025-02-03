361d76b33bbaa8765cbdfeb83481584e4ec6bec0
avabellaw <avabellawilliams@gmail.com>
2025-02-03 01:12:57 +0000

    setup: Add sqlalchemy #4

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,23 @@
 from flask import Flask
 from flask_restful import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
+
+from dotenv import load_dotenv
+
+import os
+
 
 app = Flask(__name__)
+
+load_dotenv()  # Load environment variables from .env file
+
+# Configurations
+app.secret_key = os.environ.get('SECRET_KEY')
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+
+# Initialize extensions
 api = Api(app)
+db = SQLAlchemy(app)
 
 
 class HelloWorld(Resource):
