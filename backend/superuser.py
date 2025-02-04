86ed48e977aa128341c95267da975365ffd85ae4
avabellaw <avabellawilliams@gmail.com>
2025-02-04 23:34:45 +0000

    Add superuser and tools to create + manage

---
+++ backend/superuser.py
@@ -0,0 +1,40 @@
+from flask_bcrypt import Bcrypt
+from models import db
+from models.user import User
+
+from getpass import getpass
+
+from app import app
+
+
+def create_superuser():
+    '''Create superuser'''
+    print("Create superuser:")
+
+    username = input("Username: ")
+    password = getpass("Password: ")
+
+    bcrypt = Bcrypt()
+
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    with app.app_context():
+        user = User(username=username, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+
+        print(f"Superuser '{user.username}' created successfully!")
+
+
+def create_all_tables():
+    '''Create all tables in new database'''
+    with app.app_context():
+        db.create_all()
+
+
+def drop_all_tables():
+    '''Drop all tables'''
+    anws = input("Are you sure you want to drop all tables? (y/n): ")
+    if anws == 'y':
+        with app.app_context():
+            db.drop_all()
