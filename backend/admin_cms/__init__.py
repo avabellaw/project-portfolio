9eb1bf80ed6c737d39a3e71749185390e514feb8
avabellaw <avabellawilliams@gmail.com>
2025-02-06 13:29:30 +0000

    Add project view and inline colour scheme

--- backend/admin_cms/__init__.py
+++ backend/admin_cms/__init__.py
@@ -1,9 +1,11 @@
 from flask_admin import Admin
-from .project_view import ProfileView
+
+from .project_view import ProjectView
+
 from models import db
 from models.project import Project
 
 admin = Admin(name='Porfolio Panel', template_mode='bootstrap3')
 
 
-admin.add_view(ProfileView(Project, db.session))
+admin.add_view(ProjectView(Project, db.session))
--- backend/admin_cms/project_view.py
+++ backend/admin_cms/project_view.py
@@ -1,8 +1,21 @@
 from flask_admin.contrib.sqla import ModelView
-from flask_admin.form.upload import FileUploadField
+from models.project import ProjectColourScheme
+from flask_admin.contrib.sqla.form import InlineOneToOneModelConverter
 
 
-class ProfileView(ModelView):
-    form_extra_fields = {
-        'image_url': FileUploadField('Image', base_path='uploads')
-    }
+class ProjectView(ModelView):
+    can_delete = True
+    can_create = True
+    can_edit = True
+    inline_converter = InlineOneToOneModelConverter
+
+    inline_models = (ProjectColourScheme,)
+
+    column_list = [
+        'title',
+        'description',
+        'live_url',
+        'github_url',
+        'image_url',
+        'colour_scheme'
+    ]
