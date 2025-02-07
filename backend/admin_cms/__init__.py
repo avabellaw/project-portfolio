0a9acb812f31f6fa545e1d5d03c920b2c4dae78f
avabellaw <avabellawilliams@gmail.com>
2025-02-07 14:49:27 +0000

    Add dashboard metrics and remove 'home' nav

--- backend/admin_cms/__init__.py
+++ backend/admin_cms/__init__.py
@@ -1,6 +1,6 @@
 from flask import redirect, url_for
 
-from flask_admin import Admin, AdminIndexView
+from flask_admin import Admin, AdminIndexView, expose
 from flask_admin.menu import MenuLink
 
 from flask_login import current_user
@@ -15,6 +15,21 @@ from models.skill import Skill
 
 class AdminIndexView(AdminIndexView):
     '''Admin index view class to override admin index view'''
+
+    def is_visible(self):
+        # This view won't appear in the menu structure
+        return False
+
+    @expose('/')
+    def index(self):
+        projects = Project.query
+        total_projects = projects.count()
+        total_skills = Skill.query.count()
+        return self.render('admin/index.html',
+                           total_projects=total_projects,
+                           total_skills=total_skills,
+                           projects=projects)
+
     def is_accessible(self):
         '''Check if user is authenticated'''
         return current_user.is_authenticated
@@ -27,7 +42,7 @@ class AdminIndexView(AdminIndexView):
 admin = Admin(name='Porfolio Panel',
               template_mode='bootstrap3',
               base_template='admin/base-custom.html',
-              index_view=AdminIndexView(name=None))
+              index_view=AdminIndexView(name='Home'))
 
 admin.add_link(MenuLink(name='Logout', url='logout'))
 
