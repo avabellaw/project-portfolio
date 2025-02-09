2887d471b2a4269322455a019022dd49e94eef35
avabellaw <avabellawilliams@gmail.com>
2025-02-09 15:09:25 +0000

    Add strucuture for react fontend

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+function Home(){
+    return (
+        <>
+            <h1>Home, world !</h1>
+        </>
+    )
+}
+
+export default Home;
---
+++ frontend/src/components/layout/Layout.jsx
@@ -0,0 +1,17 @@
+import '../../index.css'
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <header>
+        {}
+      </header>
+      
+      <main className="main-content">
+        {children}
+      </main>
+    </>
+  )
+}
+
+export default Layout
