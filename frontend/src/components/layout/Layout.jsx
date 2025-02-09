b4ef5afc8ab508be680d8731dfb25fa072996d7d
avabellaw <avabellawilliams@gmail.com>
2025-02-09 15:28:58 +0000

    Add initial colour scheme + set colours functionailty

--- frontend/src/components/layout/Layout.jsx
+++ frontend/src/components/layout/Layout.jsx
@@ -1,17 +1,32 @@
+import { useState } from 'react'
 import '../../index.css'
+import styles from './Layout.module.css'
 
 const Layout = ({ children }) => {
-  return (
-    <>
-      <header>
-        {}
-      </header>
-      
-      <main className="main-content">
-        {children}
-      </main>
-    </>
-  )
+    const [colours, setColours] = useState({
+        primary: 'white',
+        secondary: '#f3f3f3',
+        text: 'black'
+    })
+
+    return (
+        <div
+            id={styles.mainContainer}
+            style={{
+                "--primary-colour": colours.primary,
+                "--secondary-colour": colours.secondary,
+                "--text-colour": colours.text
+            }}
+        >
+            <header>
+                { }
+            </header>
+
+            <main className="main-content">
+                {children}
+            </main>
+        </div>
+    )
 }
 
 export default Layout
