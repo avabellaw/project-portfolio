cd43a9ec637dec7fcf9f4bc4f6bae5841e3c6d9e
avabellaw <avabellawilliams@gmail.com>
2025-02-12 02:07:10 +0000

    Add scroll wheel event listener to project nav

--- frontend/src/components/home/project/card-components/ProjectNav.jsx
+++ frontend/src/components/home/project/card-components/ProjectNav.jsx
@@ -1,6 +1,26 @@
+import { useEffect, useCallback } from 'react'
+
 import styles from './ProjectNav.module.css'
 
 const Nav = ({ setIndex, currentIndex, projects }) => {
+    const handleScroll = useCallback((event) => {
+        if (event.deltaY > 0) {
+            // Scrolling down
+            if (currentIndex === projects.length - 1) return;
+            setIndex(Math.min(currentIndex + 1));
+        } else {
+            // Scrolling up
+            if (currentIndex === 0) return;
+            setIndex(currentIndex - 1);
+        }
+    }, [currentIndex, projects.length, setIndex]);
+
+    useEffect(() => {
+        // Passive event listener to improve scrolling performance, as not calling preventDefault
+        window.addEventListener('wheel', handleScroll, { passive: true });
+        return () => window.removeEventListener('wheel', handleScroll);
+    }, [handleScroll]);
+
     return (
         <nav id={styles['nav-container']}>
             <div id={styles['nav-link-names']}>
