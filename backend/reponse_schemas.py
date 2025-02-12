b2615374fb8578eb9376361d0de5c47cb0cb8f43
avabellaw <avabellawilliams@gmail.com>
2025-02-12 18:02:21 +0000

    backend: Add text_highlight_colour to response schema

--- backend/reponse_schemas.py
+++ backend/reponse_schemas.py
@@ -3,7 +3,8 @@ from flask_restful import fields
 colour_scheme_fields = {
     'primary_colour': fields.String,
     'secondary_colour': fields.String,
-    'text_colour': fields.String
+    'text_colour': fields.String,
+    'text_highlight_colour': fields.String
 }
 
 skill_fields = {
