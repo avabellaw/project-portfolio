b17f916a10c8cd8de93d4906de3c450b7f544615
avabellaw <avabellawilliams@gmail.com>
2025-02-12 18:23:58 +0000

    Create project colour scheme context api

---
+++ frontend/src/components/layout/ColourSchemeContext.js
@@ -0,0 +1,16 @@
+import { createContext, useState } from 'react';
+
+const ColourSchemeContext = createContext();
+
+const ColourSchemeProvider = ({ children }) => {
+    const [colours, setColours] = useState({
+        primary_colour: 'white',
+        secondary_colour: 'gray',
+        text_colour: 'black',
+        text_highlight_colour: 'gold'
+    });
+
+    return <ColourSchemeContext.Provider value={{ colours, setColours }}>{children}</ColourSchemeContext.Provider>;
+};
+
+export { ColourSchemeContext, ColourSchemeProvider };
