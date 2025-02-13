036f6c81d46e510091415853c314a9f4feed007d
avabellaw <avabellawilliams@gmail.com>
2025-02-13 20:01:43 +0000

    admin: Add actions to move Project view order up/down #18

--- backend/admin_cms/project_view.py
+++ backend/admin_cms/project_view.py
@@ -1,17 +1,21 @@
-from .model_view_override import ModelView
-
-from models.project import ProjectColourScheme
 from flask_admin.contrib.sqla.form import InlineOneToOneModelConverter
 from flask_admin.model.form import InlineFormAdmin
 from flask_admin.form import FileUploadField
-from models.skill import Skill
 from flask_admin.contrib.sqla.fields import CheckboxListField
+from flask_admin.actions import action
+
 from wtforms.validators import DataRequired
 
 from io import BytesIO
 from cloudinary import uploader
 
 import os
+
+from .model_view_override import ModelView
+
+from models.skill import Skill
+from models.project import ProjectColourScheme, Project
+from models import db
 
 
 class CloudinaryUploadField(FileUploadField):
@@ -31,6 +35,12 @@ class ColourSchemeInlineView(InlineFormAdmin):
 
 class ProjectView(ModelView):
     inline_models = (ColourSchemeInlineView(ProjectColourScheme),)
+
+    actions = ['move_up', 'move_down']
+
+    def get_query(self):
+        return self.session.query(self.model
+                                  ).order_by(self.model.view_order.asc())
 
     form_extra_fields = {
         'skills': CheckboxListField(
@@ -66,13 +76,14 @@ class ProjectView(ModelView):
     ]
 
     column_list = [
+        'view_order',
         'title',
         'description',
         'live_url',
         'github_url',
         'image_url',
         'colour_scheme',
-        'skills'
+        'skills',
     ]
 
     def on_model_change(self, form, model, is_created):
@@ -83,3 +94,36 @@ class ProjectView(ModelView):
             if model.image_url:
                 model.delete_cloudinary_image()
             model.image_url = result['secure_url']
+
+    @action('move_up', 'Move Up')
+    def action_move_up(self, ids):
+        '''Move project order up in view order'''
+
+        # Allows for multiple projects to be moved up
+        for id in ids:
+            project = Project.query.get(id)
+            if project.view_order > 1:
+                prev_project = Project.query.filter(
+                    Project.view_order < project.view_order
+                ).order_by(Project.view_order.desc()).first()
+
+                if prev_project:
+                    prev_project.view_order += 1
+                    project.view_order -= 1
+                    db.session.commit()
+
+    @action('move_down', 'Move Down')
+    def action_move_down(self, ids):
+        '''Move project order down in view order'''
+
+        # Allows for multiple projects to be moved down
+        for id in ids:
+            project = Project.query.get(id)
+            next_project = Project.query.filter(
+                Project.view_order > project.view_order
+            ).order_by(Project.view_order).first()
+
+            if next_project:
+                next_project.view_order -= 1
+                project.view_order += 1
+                db.session.commit()
