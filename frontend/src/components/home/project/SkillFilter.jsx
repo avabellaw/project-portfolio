32c5c3439e14089bb5a6b2f831abf04dce61dccf
avabellaw <avabellawilliams@gmail.com>
2025-02-13 15:45:59 +0000

    style: Add filter image to skill filter placeholder text
    Additionally, minor ui improvements

--- frontend/src/components/home/project/SkillFilter.jsx
+++ frontend/src/components/home/project/SkillFilter.jsx
@@ -30,27 +30,33 @@ const Filter = ({ ALL_PROJECTS, setProjects }) => {
                 const filteredProjects = ALL_PROJECTS.filter(project =>
                     project.skills.some(skill => skill.name === option.label)
                 );
-    
+
                 if (filteredProjects.length === 0) {
                     // If no projects have the selected skill, show all projects
                     setProjects(ALL_PROJECTS);
                     return;
                 }
-    
+
                 setProjects(filteredProjects);
                 break;
             default:
                 setProjects(ALL_PROJECTS);
         }
-        console.log(actionMeta.action);
     };
+
+    const formatPlaceholder = () => (
+        <div id={styles['placeholder']}>
+            <img src="/icons/sort.png" alt="Filter" />
+            <span>Filter by skill</span>
+        </div>
+    );
 
     return (
         <div id={styles['skill-filter-container']}>
             <Select
                 options={skills}
                 onChange={handleChange}
-                placeholder="Filter by skill"
+                placeholder={formatPlaceholder()}
                 classNamePrefix="skills-filter"
                 isClearable={true}
             />
