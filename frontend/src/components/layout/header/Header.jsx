6c6155c92953210e32ca05fed52f3de841e801af
avabellaw <avabellawilliams@gmail.com>
2025-02-23 23:06:34 +0000

    Add enter animation to the header text

--- frontend/src/components/layout/header/Header.jsx
+++ frontend/src/components/layout/header/Header.jsx
@@ -1,13 +1,24 @@
 import styles from './Header.module.css'
 import { Link } from 'react-router-dom'
 
+import { motion } from 'motion/react'
+
 const Header = () => {
     return (
         <Link to="/" id={styles['header-link']}>
-            <div id={styles['header-container']}>
-                <span id={styles['name']}>Ava Williams</span>
-                <span id={styles['secondary']}>Portfolio projects</span>
-            </div>
+            <motion.div
+                initial={{ y: -50 }}
+                animate={{ y: 0 }}
+                transition={{
+                    type: 'spring',
+                    stiffness: 100,
+                    damping: 20
+                }}>
+                <div id={styles['header-container']}>
+                    <span id={styles['name']}>Ava Williams</span>
+                    <span id={styles['secondary']}>Portfolio projects</span>
+                </div>
+            </motion.div>
         </Link>
     )
 }
