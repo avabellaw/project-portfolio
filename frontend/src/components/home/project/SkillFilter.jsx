6355ed38393350c86867d67a1436598253615b48
Ava Bella Williams <avabellawilliams@gmail.com>
2025-09-29 22:08:42 +0100

    style: Fix centering of skills filter placeholder svg + text

--- frontend/src/components/home/project/SkillFilter.jsx
+++ frontend/src/components/home/project/SkillFilter.jsx
@@ -55,13 +55,21 @@ const Filter = ({ selectedValue, filterProjectsBySkill }) => {
         /**
          * Adds the filter image icon to the placeholder
          */
-        <div id={styles['placeholder']}>
+        <>
             {/* SVG embedded in html for reliability and to set fill colour in CSS */}
-            <svg width="1.1em" height="1.1em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path fillRule="evenodd" clipRule="evenodd" d="M3 7C3 6.44772 3.44772 6 4 6H20C20.5523 6 21 6.44772 21 7C21 7.55228 20.5523 8 20 8H4C3.44772 8 3 7.55228 3 7ZM6 12C6 11.4477 6.44772 11 7 11H17C17.5523 11 18 11.4477 18 12C18 12.5523 17.5523 13 17 13H7C6.44772 13 6 12.5523 6 12ZM9 17C9 16.4477 9.44772 16 10 16H14C14.5523 16 15 16.4477 15 17C15 17.5523 14.5523 18 14 18H10C9.44772 18 9 17.5523 9 17Z" fill="#000000" />
+            <svg
+                height="1.2em" /* sibling span must have same line-height*/
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                    <path fillRule="evenodd" clipRule="evenodd" d="M3 7C3 6.44772 3.44772 6 4 6H20C20.5523 6 21 6.44772 21 7C21 7.55228 20.5523 8 20 8H4C3.44772 8 3 7.55228 3 7ZM6 12C6 11.4477 6.44772 11 7 11H17C17.5523 11 18 11.4477 18 12C18 12.5523 17.5523 13 17 13H7C6.44772 13 6 12.5523 6 12ZM9 17C9 16.4477 9.44772 16 10 16H14C14.5523 16 15 16.4477 15 17C15 17.5523 14.5523 18 14 18H10C9.44772 18 9 17.5523 9 17Z" fill="#000000" />
             </svg>
-            <span>Filter by skill</span>
-        </div>
+
+            {/* line-height value must match height of svg */}
+            <span
+                style={{lineHeight: "1.2em"}}
+            >Filter by skill</span>
+        </>
     );
 
     const handleInputChanged = (input) => {
